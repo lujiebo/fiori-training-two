b298d48d463b464dcf03fb3512d046292fbc29b4
lujiebo <lujb_cx@sina.com>
2020-05-12 17:07:28 +0800

    git

--- WebContent/controller/FeeList.controller.js
+++ WebContent/controller/FeeList.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-], function(BaseController, JSONModel,  Filter, FilterOperator) {
+git], function (BaseController, JSONModel, Filter, FilterOperator) {
 	"use strict";
 
 	return BaseController.extend("com.sap.sdrive.ui.fee.controller.FeeList", {
@@ -25,23 +25,27 @@ sap.ui.define([
 
 			var that = this;
 
-            this.getView().byId("feeTable").bindElement({
-                path : "/Fee",
-                model: "DrivingSafety",
-                parameters: {
-                    expand: "Vehicle"
-                },
-                events: {
-                    dataReceived: function(response) {
-                        // if (response.mParameters.data.Message !== '') {
-                        //     sap.m.MessageToast.show(that.getI18NText("OPERATION_FAILED"));
-                        // }
-                    }
-                }
+			this.getView().byId("feeTable").bindElement({
+				path: "/Fee",
+				model: "DrivingSafety",
+				parameters: {
+					expand: "Vehicle"
+				},
+				events: {
+					dataReceived: function (response) {
+						// if (response.mParameters.data.Message !== '') {
+						//     sap.m.MessageToast.show(that.getI18NText("OPERATION_FAILED"));
+						// }
+					}
+				}
 			});
-		
+
 		},
-		onChangeDateRange: function(oEvent) {
+		onChangeFeeStart: function () {
+			var iFeeStart = Number(this.getView().byId("fee_start").getValue());
+
+		},
+		onChangeDateRange: function (oEvent) {
 			var bValid = oEvent.getParameter("valid"),
 				oEventSource = oEvent.getSource();
 			if (bValid) {
@@ -50,7 +54,7 @@ sap.ui.define([
 				oEventSource.setValueState(sap.ui.core.ValueState.Error);
 			}
 		},
-        onClear: function (oEvent) {
+		onClear: function (oEvent) {
 			var oSearch = {
 				plateNumberKeys: [],
 				feeStartDateFrom: null,
@@ -65,7 +69,7 @@ sap.ui.define([
 
 			this.getModel("searchModel").setData(oSearch);
 		},
-		onSearch: function(oEvent) {
+		onSearch: function (oEvent) {
 			var oSearchModel = this.getModel("searchModel"),
 				oSearch = oSearchModel.getProperty("/"),
 				aPlateNumberFilters = [],
@@ -75,10 +79,10 @@ sap.ui.define([
 				aFeeStartFilters = [],
 				aFeeEndFilters = [],
 				aFilterGroups = [];
-	
-				
+
+
 			if (oSearch.plateNumberKeys && oSearch.plateNumberKeys.length > 0) {
-				for (var i = 0; i < oSearch.plateNumberKeys.length; i ++) {
+				for (var i = 0; i < oSearch.plateNumberKeys.length; i++) {
 					aPlateNumberFilters.push(new Filter(
 						"PLATE_NUMBER",
 						FilterOperator.EQ,
@@ -88,7 +92,7 @@ sap.ui.define([
 				aFilterGroups.push(new Filter(aPlateNumberFilters, false));
 			}
 
-			
+
 
 			if (oSearch.feeStartDateFrom && oSearch.feeStartDateTo) {
 				aFeeStartDateFilters.push(new Filter(
@@ -105,7 +109,7 @@ sap.ui.define([
 				aFilterGroups.push(new Filter(aFeeStartDateFilters, true));
 			}
 
-			
+
 
 			if (oSearch.feeEndDateFrom && oSearch.feeEndDateTo) {
 				aFeeEndDateFilters.push(new Filter(
@@ -122,7 +126,7 @@ sap.ui.define([
 				aFilterGroups.push(new Filter(aFeeEndDateFilters, true));
 			}
 
-			
+
 			if (oSearch.changedOnFrom && oSearch.changedOnTo) {
 				aChangedOnFilters.push(new Filter(
 					"CHANGED_ON",
@@ -137,55 +141,58 @@ sap.ui.define([
 
 				aFilterGroups.push(new Filter(aChangedOnFilters, true));
 			}
-			
-			if(Number(oSearch.feeStart)>0){
+
+			var iFeeStart = this.getView().byId("fee_start").getValue();
+			var iFeeEnd = this.getView().byId("fee_end").getValue();
+			if (iFeeStart && iFeeStart.length > 0) {
 				aFeeStartFilters.push(new Filter(
 					"FEE",
 					FilterOperator.GE,
-					oSearch.feeStart
+					iFeeStart
 				));
-				aFilterGroups.push(new Filter(aFeeStartFilters,true));
+				aFilterGroups.push(new Filter(aFeeStartFilters, true));
 			}
 
-			if(oSearch.feeEnd!=null){
+
+			if (iFeeEnd && iFeeEnd.length > 0) {
 				aFeeEndFilters.push(new Filter(
 					"FEE",
 					FilterOperator.LT,
-					oSearch.feeEnd
+					iFeeEnd
 				));
 
-				aFilterGroups.push(new Filter(aFeeEndFilters,true));
+				aFilterGroups.push(new Filter(aFeeEndFilters, true));
 			}
 
 			var oBindingItems = this.getView().byId("feeTable").getBinding("items");
-		
+
 			if (aFilterGroups && aFilterGroups.length > 0) {
 				var oGroupFilter = new Filter(aFilterGroups, true);
 				oBindingItems.filter(oGroupFilter);
 			} else {
 				oBindingItems.filter([]);
 			}
-			
-			
+
+
 		},
-		onFeeSortButtonPressed: function(oEvent) {
-            var _oFeeSortSettingsDialog = sap.ui.xmlfragment("com.sap.sdrive.ui.fee.view.fragment.FeeSortDialog", this);
-            this._oFeeSortSettingsDialog = _oFeeSortSettingsDialog;
-            this.getView().addDependent(this._oFeeSortSettingsDialog);
-            this._oFeeSortSettingsDialog.open();
+		onFeeSortButtonPressed: function (oEvent) {
+			var _oFeeSortSettingsDialog = sap.ui.xmlfragment("com.sap.sdrive.ui.fee.view.fragment.FeeSortDialog", this);
+			this._oFeeSortSettingsDialog = _oFeeSortSettingsDialog;
+			this.getView().addDependent(this._oFeeSortSettingsDialog);
+			this._oFeeSortSettingsDialog.open();
 		},
-		handleFeeSortDialogConfirm: function(oEvent) {
-            var oTable = this.byId("feeTable"),
-                mParams = oEvent.getParameters(),
-                oBinding = oTable.getBinding("items"),
-                sPath,
-                bDescending,
-                aSorters = [];
-            
-            sPath = mParams.sortItem.getKey();
-            bDescending = mParams.sortDescending;
-            aSorters.push(new sap.ui.model.Sorter(sPath, bDescending));
-            oBinding.sort(aSorters);
+		handleFeeSortDialogConfirm: function (oEvent) {
+			var oTable = this.byId("feeTable"),
+				mParams = oEvent.getParameters(),
+				oBinding = oTable.getBinding("items"),
+				sPath,
+				bDescending,
+				aSorters = [];
+
+			sPath = mParams.sortItem.getKey();
+			bDescending = mParams.sortDescending;
+			aSorters.push(new sap.ui.model.Sorter(sPath, bDescending));
+			oBinding.sort(aSorters);
 		}
 	});
 
